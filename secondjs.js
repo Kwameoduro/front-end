c7d8062d2746cef0be578fd07d0744055be7386f
Kwameoduro <odurokwameee@gmail.com>
2023-11-19 21:37:36 +0000

    add: introduction to  variables.

---
+++ secondjs.js
@@ -0,0 +1,30 @@
+var petdog = "Rex";
+var petcat = "Pepper";
+console.log(petdog);
+console.log(petcat);
+console.log("My pet dog's name is: ", petdog);
+console.log("My pet cat's name is:", petcat);
+
+var catSound = "purr";
+var dogSound = "woof";
+
+console.log(petcat, "says", catSound);
+console.log(petdog, "says", dogSound);
+
+catSound = "meow";
+console.log(petcat, "now says", catSound);
+
+
+/*
+output:
+Rex
+Pepper
+My pet dog's name is: Rex
+My pet cat's name is: Pepper
+
+Pepper says purr
+Rex says woof
+
+Pepper now says meow
+
+*/
