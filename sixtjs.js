a1f9f2531ed06d089cb6cb38c3abf090c602dedf
Kwameoduro <odurokwameee@gmail.com>
2023-11-20 23:36:23 +0000

    add: conditional statements (switch)

---
+++ sixtjs.js
@@ -0,0 +1,26 @@
+var day = "Sunday";
+
+switch(day){
+    case 'Monday':
+        console.log("Do something");
+        break;
+    case 'Tuesday':
+        console.log("Do anything");
+        break;
+    case 'Wednesday':
+        console.log("Men, isn't it weekends yet?");
+        break;
+    case 'Thurday':
+        console.log("Exploring mars in my sleep");
+        break;
+    case 'Friday':
+        console.log("Already there!");
+        break;
+    case 'Saturday':
+        console.log("Send me location");
+        break;
+    case 'Sunday':
+        console.log("Monday came early");
+        break;
+
+}
