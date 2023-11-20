5e8fad97d013f4939c8c832c936e7a39f5104e94
Kwameoduro <odurokwameee@gmail.com>
2023-11-20 23:19:02 +0000

    add: conditional statements

---
+++ fifthjs.js
@@ -0,0 +1,17 @@
+var age = 10;
+
+if (age >= 60) {
+    console.log("You get your income from your pension")
+} 
+else if (age <= 60 & age >= 18){
+    console.log("Each month you get a salary")
+}
+else if ( age < 18){
+    console.log("You get an allowance")
+}
+else {
+    console.log("he value of the age variable is not numerical")
+}
+
+// output
+// You get an allowance
