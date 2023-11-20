d7bc5c6cc8f8935179710be169c904d51449acfa
Kwameoduro <odurokwameee@gmail.com>
2023-11-20 00:20:09 +0000

    add: introduction to operators

---
+++ thirdjs.js
@@ -0,0 +1,40 @@
+var currentTime = 10;
+console.log(currentTime > 9 && currentTime < 17); // true
+
+
+
+var currentTime = 7;
+console.log(currentTime < 9 || currentTime > 17); // true
+
+
+console.log(22 % 5); // 2
+
+console.log(5 !== "5") // true
+console.log(5 !== 5) // false
+
+
+
+var mon = 1;
+var tue = 2;
+var wed = 1;
+var thu = 2;
+var fri = 3;
+console.log(mon + tue + wed + thu + fri); // 9
+
+
+var overtime = 1;
+overtime += 2;
+overtime += 1;
+overtime += 2;
+overtime += 3;
+console.log(overtime); // 9
+
+
+var longString = "";
+longString += "Once";
+longString += " upon";
+longString += " a";
+longString += " time";
+longString += "...";
+console.log(longString); // "Once upon a time..."
+
