5f5c5f30c739944350f2e7711a69ff707fb97f33
Kwameoduro <odurokwameee@gmail.com>
2023-11-21 01:31:45 +0000

    add: for loops

---
+++ seventhjs.js
@@ -0,0 +1,46 @@
+for (var i = 0; i <= 3; i++) {
+    console.log("This is line ", i)
+}
+
+// output
+// This is line  0
+// This is line  1
+// This is line  2
+// This is line  3
+
+
+
+for (var i = 1; i <= 5; i++) {
+    console.log(i);
+};
+console.log('Counting completed!');
+
+
+// output
+/* 
+1
+2
+3
+4
+5
+Counting completed!
+*/
+
+
+
+for (var i = 5; i > 0; i--) {
+    console.log(i);
+};
+console.log('Countdown finished!');
+
+
+// output
+
+/*
+5
+4
+3
+2
+1
+Countdown finished!
+*/
