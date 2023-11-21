79d38b2d5a01771b04f33b02744e4c068d5ad095
Kwameoduro <odurokwameee@gmail.com>
2023-11-21 01:40:25 +0000

    add: while loops

---
+++ eighthjs.js
@@ -0,0 +1,53 @@
+var i = 1;
+while (i < 6) {
+    console.log(i);
+    i++;
+};
+console.log('Counting completed!');
+
+// output
+/*
+1
+2
+3
+4
+5
+Counting completed!
+*/
+
+
+
+var i = 5;
+while (i > 0) {
+    console.log(i);
+    i = i - 1;
+};
+console.log('Counting completed!');
+
+// output
+/* 
+5
+4
+3
+2
+1
+Counting completed!
+*/
+
+
+
+var year = 2018;
+while (year < 2023) {
+    console.log(year);
+    year++;
+};
+
+
+// output
+/*
+2018
+2019
+2020
+2021
+2022
+*/
