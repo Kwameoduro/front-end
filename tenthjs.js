ca064a1d2bb751b72e560c7f8cc3d4665881f22f
Kwameoduro <odurokwameee@gmail.com>
2023-11-21 23:36:58 +0000

    add: uses of loops

---
+++ tenthjs.js
@@ -0,0 +1,20 @@
+var cubes = 'ABCDEFG';
+//styling console output using CSS with a %c format specifier
+for (var i = 0; i < cubes.length; i++) {
+    var styles = "font-size: 40px; border-radius: 10px; border: 1px solid blue; background: pink; color: purple";
+    console.log("%c" + cubes[i], styles)
+}
+
+
+// output
+/*
+A
+B
+C
+D
+E
+F
+G
+
+(t displays differently since a css component was added)
+*/
