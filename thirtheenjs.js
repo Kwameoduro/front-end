5c54505ee72e7792f3f1c098975fdc54c3d3d678
Kwameoduro <odurokwameee@gmail.com>
2023-11-23 05:16:47 +0000

    add: building and calling functions

---
+++ thirtheenjs.js
@@ -0,0 +1,20 @@
+function listArrayItems(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        console.log(i, arr[i])
+    }
+}
+var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink'];
+listArrayItems(colors);
+
+// output
+/*
+0 red
+1 orange
+2 yellow
+3 green
+4 blue
+5 purple
+6 pink
+*/
+
+
