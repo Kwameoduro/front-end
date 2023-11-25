b84d710b0a2f58d6208bf8d6b323b2a526f83220
Kwameoduro <odurokwameee@gmail.com>
2023-11-25 22:10:34 +0000

    add: arrays and objects

---
+++ twentyonejs.js
@@ -0,0 +1,10 @@
+function arrayBuilder(one, two, three) {
+    var arr = [];
+    arr.push(one);
+    arr.push(two);
+    arr.push(three);
+    console.log(arr);
+}
+arrayBuilder('apple', 'pear', 'plum'); 
+
+//output: ['apple', 'pear', 'plum']
