09714228f24c41ef97607cf61da9fb2e889bc1d1
Kwameoduro <odurokwameee@gmail.com>
2023-11-25 21:49:46 +0000

    update: object literals and bracket notation

---
+++ nineteenjs.js
@@ -0,0 +1,17 @@
+var arrOfKeys = ['speed', 'altitude', 'color'];
+var drone = {
+    speed: 100,
+    altitude: 200,
+    color: "red"
+}
+for (var i = 0; i < arrOfKeys.length; i++) {
+    console.log(drone[arrOfKeys[i]])
+}
+
+// output
+
+/*
+100
+200
+red
+*/
