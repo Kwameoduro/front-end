47786f52b213104d3ef23f97df11af8343da9171
Kwameoduro <odurokwameee@gmail.com>
2023-11-25 22:07:41 +0000

    add: introduction to array and objects.

---
+++ twentyjs.js
@@ -0,0 +1,15 @@
+var fruits = [];
+fruits.push("apple");
+console.log(fruits)
+
+// output : [ 'apple' ]
+
+fruits.push('pear'); 
+console.log(fruits)
+
+// output: [ 'apple', 'pear' ]
+
+
+fruits.pop();
+console.log(fruits); 
+// output: ['apple']
