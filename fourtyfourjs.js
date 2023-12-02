f7d846fb78d26e4907741ce34bed6b202bb8afdd
Kwameoduro <odurokwameee@gmail.com>
2023-12-02 21:35:26 +0000

    add: template literals

---
+++ fourtyfourjs.js
@@ -0,0 +1,12 @@
+let greet = "Hello";
+let place = "World";
+console.log(`${greet} ${place} !`) 
+//output: Hello World !
+
+var greet1 = "Hello";
+var place1 = "World";
+console.log(greet1 + " " + place1 + "!"); 
+// output: Hello World!
+
+console.log(`${1 + 1 + 1 + 1 + 1} stars!`) 
+// output: 5 stars!
