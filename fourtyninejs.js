d1150281278d6861f1d9d9bc71e8b8ff09a0f8e4
Kwameoduro <odurokwameee@gmail.com>
2023-12-03 02:04:02 +0000

    add: advanced javascript features

---
+++ fourtyninejs.js
@@ -0,0 +1,45 @@
+const meal = ["soup", "steak", "ice cream"]
+    let [starter] = meal;
+    console.log(starter);
+
+    // output: soup
+
+
+
+let food = "Chocolate";
+console.log(`My favourite food is ${food}`);
+
+// output: My favourite food is Chocolate
+
+
+let set = new Set();
+    set.add(1);
+    set.add(2);
+    set.add(3);
+    set.add(2);
+    set.add(1);
+    console.log(set);
+
+// output: Set(3) { 1, 2, 3 }
+
+
+let obj = {
+    key: 1,
+    value: 4
+    };
+
+let output = { ...obj };
+output.value -= obj.key;
+
+console.log(output.value);
+
+// output: 3
+
+
+function count(...basket) {
+    console.log(basket.length)
+}
+
+count(10, 9, 8, 7, 6);
+
+// output: 5
