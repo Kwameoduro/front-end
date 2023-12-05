1b05aa868750b94663aeeaf0df7f701b694d890f
Kwameoduro <odurokwameee@gmail.com>
2023-12-05 12:55:20 +0000

    add: jest-testing

---
+++ jest-testing/timesTwo.js
@@ -0,0 +1,7 @@
+//task 1: Code the timesTwo function declaration
+function timesTwo(number){
+    return number * 2;
+}
+
+// task 2: Export the timeTwo function as a module
+module.exports = timesTwo;
