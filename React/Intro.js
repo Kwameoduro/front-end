b6b987537758d7a7d345a678f0f3cd91210b25eb
Kwameoduro <odurokwameee@gmail.com>
2023-12-25 13:57:33 +0000

    add: intro to ternary expression

---
+++ React/Intro.js
@@ -0,0 +1,12 @@
+let name = 'Bob';
+if (name == 'Bob') {
+    console.log('Hello, Bob');
+} else {
+    console.log('Hello, Friend');
+};
+
+
+// In ternary expression
+
+let name1 = 'Bob';
+name1 == 'Bob' ? console.log('Hello, Bob') : console.log('Hello, Friend');
