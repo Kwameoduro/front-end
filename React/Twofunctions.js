23e9ab7c6759c18213d950aac6b5f6055d7e352b
Kwameoduro <odurokwameee@gmail.com>
2023-12-26 23:11:14 +0000

    add: functions in JSX

---
+++ React/Twofunctions.js
@@ -0,0 +1,11 @@
+// Function expression:
+
+const getRandomNum = function() {
+    return Math.floor(Math.random() * 10) + 1
+} ;
+
+
+// Function declaration:
+function getRandomNum() {
+    return Math.floor(Math.random() *10) + 1
+};
