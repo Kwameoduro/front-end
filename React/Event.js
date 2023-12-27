6f8d6ab76bf2915d175e2bedfefe13cb233ebcc2
Kwameoduro <odurokwameee@gmail.com>
2023-12-27 21:51:59 +0000

    add: event handling

---
+++ React/Event.js
@@ -0,0 +1,7 @@
+// Handling events using inline anonymous ES5 functions
+
+<button onClick={function() {console.log('first example')}}>
+    An inline anonymous ES5 function event handler
+</button>
+
+
