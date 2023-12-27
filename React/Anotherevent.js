7a88b4e0ed4f58d56b01c33f629e4e490f225ede
Kwameoduro <odurokwameee@gmail.com>
2023-12-27 21:53:23 +0000

    update: event handling

---
+++ React/Anotherevent.js
@@ -0,0 +1,5 @@
+// Handling events using inline anonymous ES6 functions (arrow functions)
+
+<button onClick={() => console.log('second example')}>
+    An inline anonymous ES6 function event handler
+</button>
