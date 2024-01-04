6437d35d0c2fa30260ff2bf347f232d2878920d2
Kwameoduro <odurokwameee@gmail.com>
2024-01-04 13:47:06 +0000

    add: applying conditional rendering

---
+++ React/Conditional-Rendering/CurrentMessage2.js
@@ -0,0 +1,6 @@
+function CurrentMessage(props) {
+    if (props.day >= 1 && props.day <= 5) {
+        return < Workdays />
+    }
+    return <Weekends /> 
+}
---
+++ React/Conditional-Rendering/CurrentMessage3.js
@@ -0,0 +1,24 @@
+// Conditional rendering with the help of element variables
+
+function CurrentMessage({day}) {
+    const weekday = (day >= 1 && day <= 5);
+    const weekend = (day >= 6 && day <= 7);
+    let message;
+
+
+    if (weekday) {
+        message = <Workdays />
+    }
+    else if (weekend) {
+        message = <Weekends />
+    }
+    else {
+        message = <ErrorComponent />
+    }
+
+    return (
+        <div>
+            {message}
+        </div>
+    )
+}
