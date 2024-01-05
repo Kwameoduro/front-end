7baa15c900637d30a5b2d7009a4212679e0dcb83
Kwameoduro <odurokwameee@gmail.com>
2024-01-05 11:25:20 +0000

    add: audio in video enhancement

---
+++ React/Audio-Component.js
@@ -0,0 +1,18 @@
+import './App.css'
+import ReactPlayer from 'react-player';
+
+function App () {
+    const vidUrl = "https://www.facebook.com/facebook/videos/10153231379946729/";
+
+    return (
+        <div className = 'App'>
+            <h1> React Player example</h1>
+            <ReactPlayer
+                url = {vidUrl}
+                playing = {false}
+                volume = {0.5}
+            />
+        </div>
+
+    );
+}
