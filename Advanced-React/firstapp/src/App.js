feb91f0b9cb53c7cce8f7efd60d5ef9a4aff8b6d
Kwameoduro <odurokwameee@gmail.com>
2024-01-09 11:05:01 +0000

    add: transforming list in javascript

---
+++ Advanced-React/firstapp/src/App.js
@@ -0,0 +1,36 @@
+const data = [
+  {
+    id: "1",
+    title: "Tiramisu",
+    description: "The best tiramisu in town",
+    image : "https://picsum.photos/200/300/?random",
+    price: "$5.00",
+  },
+  {
+    id: "2",
+    title: "Lemon Ice Cream",
+    description: "Mind blowing taste",
+    image: "https://picsum.photos/200/300/?random",
+    price: "$4.50",
+  },
+  {
+    id: "3",
+    title: "Chocolate mousee",
+    description: "Unexplored flavour",
+    image: "https://picsum.photos/200/300/?random",
+    price: "$6.00",
+
+  }
+];
+
+const topDesserts = data.map(dessert => {
+  return {
+    content: '${dessert.title} - ${dessert.description}',
+    price: dessert.price,
+  }
+})
+
+export default function App() {
+  console.log(topDesserts);
+  return <h1>Examine the console output</h1>;
+}
