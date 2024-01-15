79441f8dd9eb2b94e628461978e6590e467b4488
Kwameoduro <odurokwameee@gmail.com>
2024-01-15 23:41:04 +0000

    add: useMemo

---
+++ React-Context/Four.js
@@ -0,0 +1,18 @@
+const App = () => {
+      const a = 'hi';
+      const b = 'bye';
+      const value = useMemo(() => ({a, b}), [a, b]);
+    
+      return (
+        <AppContext.Provider value={value}>
+          <ComponentA />
+        </AppContext.Provider>
+      );
+    };
+    
+    const ComponentA = React.memo(() => <ComponentB />);
+    const ComponentB = () => <ComponentC />;
+    const ComponentC = () => {
+      const contextValue = useContext(AppContext);
+      return null;
+    };
