dac27daae20f0b74482161348a26dd99dcfee2b5
Kwameoduro <odurokwameee@gmail.com>
2024-01-30 00:06:49 +0000

    update: useRef to access underlying DOM

--- Data-Fetching/Try3.js
+++ Data-Fetching/Try3.js
@@ -3,8 +3,21 @@ import React from "react";
 import './App.css';
 
 function App(){
-    const formInputef = React.useRef(null);
+    const formInpuRef = React.useRef(null);
+
+
     const focusInput = () => {
-        
+        formInpuRef.current.focus();
+
     }
+
+    return (
+        <>
+            <h1>Using useRef to access underlying DOM</h1>
+            <input ref={formInpuRef} type="text" />
+            <button onClick={focusInput}>focus Input</button>
+        </>
+    );
 }
+
+export default App;
