1d585f5c3b988586ebaf8f87e1ed6b3ec57d971c
Kwameoduro <odurokwameee@gmail.com>
2024-02-02 23:42:32 +0000

    add: types of children

---
+++ JSX/Second.js
@@ -0,0 +1,16 @@
+// explicitness 
+return (
+      <React.Fragment>
+        <li>Pizza margarita</li>
+        <li>Pizza diavola</li>
+      </React.Fragment>
+    );
+
+
+// shorter syntax
+return (
+  <>
+    <li>Pizza margarita</li>
+    <li>Pizza diavola</li>
+  </>
+);
