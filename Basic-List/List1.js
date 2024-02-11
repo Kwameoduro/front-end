73561cbbd2f3311f198a59f4f47c311c2ef37992
Kwameoduro <odurokwameee@gmail.com>
2024-02-11 23:52:03 +0000

    add: create a basic list

---
+++ Basic-List/List1.js
@@ -0,0 +1,16 @@
+const lowCaloriesDesserts = props.data 
+ .filter((dessert) => { 
+   return dessert.calories < 500; 
+ }) 
+ .sort((a, b) => { 
+   return a.calories - b.calories; 
+ }) 
+ .map((dessert) => { 
+   return ( 
+     <li> 
+       {dessert.name} - {dessert.calories} cal 
+     </li> 
+   ); 
+ }); 
+
+ 
